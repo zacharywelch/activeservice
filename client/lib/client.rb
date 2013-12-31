0f9f3f7952098fdacea4d0b1e220c4fcc54636fa
zwelchcb <Zachary.Welch@careerbuilder.com>
2013-12-31 09:55:37 -0500

    client.rb out of sync

--- client/lib/client.rb
+++ client/lib/client.rb
@@ -1,23 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'active_attr'
-require 'client/persistence'
-
-I18n.enforce_available_locales = true
-
-# = ActiveService
-#
-# ActiveService extends ActiveAttr w/ a persistence mechanism
-# using Typhoeus 
-#
-# See the documentation for ActiveAttr and Typhoeus for details on these gems 
-#
-# For ActiveService examples see README.md
-module ActiveAttr
-  module Model
-    include Persistence
-  end
-end
-
-# examples 
+require 'client/model'
 require 'client/user'
 require 'client/security_tag'
+
+I18n.enforce_available_locales = true
