6f3714bd20e542861107e18bc4f71c4ad4ef424f
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-02-05 13:10:23 -0500

    remove client folder

--- client/lib/client.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'active_attr'
-require 'client/model'
-require 'client/user'
-require 'client/security_tag'
-require 'client/invoice'
-
-I18n.enforce_available_locales = true
