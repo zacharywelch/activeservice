9e5244c41ff2797485dbcadaf231b63fac595bdc
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-02-05 16:47:56 -0500

    exclude user and invoice from gem

--- active_service/lib/active_service.rb
+++ active_service/lib/active_service.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'active_attr'
 require 'active_service/model'
-require 'active_service/user'
-require 'active_service/invoice'
+# require 'active_service/user'
+# require 'active_service/invoice'
 
 I18n.enforce_available_locales = true
--- active_service/lib/active_service/user.rb
+++ active_service/lib/active_service/user.rb
@@ -2,7 +2,6 @@ class User
   include ActiveAttr::Model
 
   self.base_uri = "http://localhost:3000/api/v1/users"
-  self.headers  = { Authorization: "Partner careerbuilder:1n73rnal" }
 
   attribute :name 
   attribute :email
@@ -14,3 +13,15 @@ class User
 
   before_save { |user| user.email = user.email.downcase }  
 end
+
+# ActiveRecord equivalent
+
+# class User < ActiveRecord::Base
+  
+#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+#   validates :name,  presence: true, length: { maximum: 50 }
+#   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }  
+
+#   before_save { |user| user.email = user.email.downcase }    
+# end
