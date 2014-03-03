fff31e2b3424009a8aee40f19cda5b1194d1c6cb
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-03-03 12:04:42 -0500

    remove invoice reference from gem

--- lib/active_service.rb
+++ lib/active_service.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'active_service/config'
 require 'active_service/model'
 # require 'active_service/user'
-require 'active_service/invoice'
+# require 'active_service/invoice'
 
 I18n.enforce_available_locales = true
