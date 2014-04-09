7abc5317c5922c03c271fa7a8c1a411ec94cee41
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-04-09 16:07:14 -0400

    move models to spec/dummy

--- spec/dummy/user.rb
+++ spec/dummy/user.rb
@@ -15,28 +15,10 @@ class User < ActiveService::Base
 end
 
 
-# class User
-#   include ActiveService::Model
-
-#   self.base_uri = "http://localhost:3000/api/v1/users"
-
-#   attribute :name, field: 'name'
-#   attribute :email, field: 'email'
+# class User < ActiveRecord::Base
   
 #   has_many :microposts
 
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-#   validates :name,  presence: true, length: { maximum: 50 }
-#   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }  
-
-#   before_save { |user| user.email = user.email.downcase }  
-# end
-
-# ActiveRecord equivalent
-
-# class User < ActiveRecord::Base
-  
 #   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 #   validates :name,  presence: true, length: { maximum: 50 }
