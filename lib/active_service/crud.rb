f1400207a065aef1e3de71da3a420ac06abf6283
zwelchcb <Zachary.Welch@careerbuilder.com>
2014-06-23 17:14:56 -0400

    rollback verifypeer for testing

--- lib/active_service/crud.rb
+++ lib/active_service/crud.rb
@@ -314,7 +314,8 @@ module ActiveService
       private
 
         def default_options
-          { headers: headers, ssl_verifypeer: ActiveService::Config.verify_ssl }
+          { headers: headers }
+          # , ssl_verifypeer: ActiveService::Config.verify_ssl }
         end
 
         def instantiate_record(record)
